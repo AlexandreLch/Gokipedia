5f6431f7605a08259acbd9bd45856fa06e48fddf
AlexandreLch <alexandre.lellouche@hetic.net>
2020-07-22 15:33:28 +0200

    Add: basic MVC hello world

--- app/controllers/home.go
+++ app/controllers/home.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"fmt"
+	"gokipedia/app/models"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func RenderHome(w http.ResponseWriter, r *http.Request) {
+	message := models.Message{Greeting: "Yo frr"}
+	tpl := template.New("home.html")                   // Create a template.
+	t, err := tpl.ParseFiles("go/templates/home.html") // Parse template file.
+	if err != nil {
+		log.Print(err)
+		path, err := os.Getwd()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(path) // for example /home/user
+		return
+	}
+	err = t.Execute(w, message) // merge.
+	if err != nil {
+		log.Print(err)
+		path, err := os.Getwd()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(path) // for example /home/user
+		return
+	}
+}
--- app/main.go
+++ app/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"gokipedia/app/router"
 	"log"
 	"net/http"
 )
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
-}
-
 func main() {
-	http.HandleFunc("/", HelloServer)
+	newRouter := router.NewRouter()
 	port := "8080"
-	log.Print("\nServer started on port "+port)
-	err := http.ListenAndServe(":"+port, nil)
+	log.Print("\nServer started on port " + port)
+	err := http.ListenAndServe(":"+port, newRouter)
 	if err != nil {
 		log.Fatalf("could not serve on port %s", port)
 	}
