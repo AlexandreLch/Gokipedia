27d39c620b62d60fb1d295ee863abd866ff7f181
Athenais Dussordet <ma.dussordet@gmail.com>
2020-07-22 14:32:49 +0200

    Change: update desc on .sql data strucutre

--- docker/data/database.sql
+++ docker/data/database.sql
@@ -2,10 +2,10 @@
     This file is used by the docker-compose build command to build the mysql db
     
     Tables:
-    * category : stores categories (id, name, desc, creation, update)
-    * image : stores images (id, name, desc, type, creation, update, category ID)
-    * tag : stores tags (id, name, creation date)
-    * image_tag : links images to tags by ids (Many to Many relation)
+    * article : stores articles (id, title, header, authors, created_on, updated_on)
+    * section : stores sections (id, title, paragraph, position, media, created_on, updated_on,
+    parent_id, article_id)
+    * comment : stores comments (id, title, content, author, created_on, updated_on)
 */
 
 CREATE TABLE article (
