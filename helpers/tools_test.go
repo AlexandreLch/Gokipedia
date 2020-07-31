cc6b8449c3ab17187e8a14531db05cfa534bd7c3
Raineau Thomas <thomasraineau3@gmail.com>
2020-07-31 11:13:54 +0200

    Add: Working unit test

--- helpers/tools_test.go
+++ helpers/tools_test.go
@@ -1,13 +1,19 @@
 package helpers
 
 import (
-	"github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"testing"
+	"fmt"
 )
 
 func TestParseUInt64(t *testing.T){
 	g := NewGomegaWithT(t)
 
 	stringToUint, err := ParseUInt64("5")
+
+	if err != nil {
+		fmt.Errorf("could not parse string to int")
+	}
 	
-	g.Expect(stringToUint).To(BeString(), "String should have become a int")
+	g.Expect(stringToUint).To(BeEquivalentTo(5), "String should have become a int")
 }
